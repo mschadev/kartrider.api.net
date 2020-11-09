a50a8e1bb4a80fd3498b91a8799c045f61792719
Plorence <ploseok@gmail.com>
2020-11-09 20:55:38 +0900

    메타데이터 싱글톤 패턴 클래스 추가

---
+++ Kartrider.API.Tests/MetadataSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kartrider.API.Tests
+{
+    class MetadataSingleton
+    {
+        public static Metadata Metadata { get; private set; }
+        static MetadataSingleton()
+        {
+            Metadata = new Metadata(true);
+        }
+    }
+}
